8f4c0d647ed4ec84e56b17fa4308f28c6ae2cd38
MHuarcayan <161988155+MHuarcayan@users.noreply.github.com>
2024-03-13 01:38:35 +0000

    Modulo2

---
+++ Modulo2/scripts/bucles.py
@@ -0,0 +1,29 @@
+# Bucle --> Una tarea que se hace repetitiva 
+
+
+# Ejemplo: imprime hola 10 veces
+
+# print('Hola')
+# print('Hola')
+# print('Hola')
+# print('Hola')
+# print('Hola')
+# print('Hola')
+# print('Hola')
+# print('Hola')
+# print('Hola')
+# print('Hola')
+
+# Bucle For --> repite uina tarea cierto numero de veces, 
+#               navega a travez de una coleccion de datos para repetir la tarea 
+
+# for _ in range(10):
+#    print('hola')
+
+# Bucle While --> Va arealizar una accion siempre y cuando la condicion a evaluar sea verdad 
+    
+n=1
+while n<=10:
+    print('hola')
+    n=n+1
+    
